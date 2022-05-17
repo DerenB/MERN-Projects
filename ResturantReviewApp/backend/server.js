7f4ec064671a481c48ef78e4b6c9ede822058cf3
Deren B <dbozerprogramming@gmail.com>
2022-05-16 22:07:13 -0400

    Connecting to server

---
+++ ResturantReviewApp/backend/server.js
@@ -0,0 +1,19 @@
+// Import items
+import express from "express"
+import cors from "cors"
+import resturants from "./api/resturants.route.js"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+// Some of the initial routes
+// Main URL
+app.use("/api/v1/restaurants", resturants)
+
+// Message for a route that doesn't exist
+app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
+
+// Exporting the app as a module
+export default app
